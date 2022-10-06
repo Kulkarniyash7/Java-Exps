ecdb0dec460edadf71aa944524ecbf316996defc
Yash Kulkarni <88444835+Kulkarniyash7@users.noreply.github.com>
2022-10-05 21:39:52 -0700

    Reuplodaded things to be updated

---
+++ MathTest.java
@@ -0,0 +1,14 @@
+import mymath.Arithmetic;
+import java.util.*;
+public class MathTest {
+	public static void main(String[]args){
+	
+		Arithmetic A = new Arithmetic();
+		A.Addition();
+		A.Substraction();
+	
+	}
+	
+	
+}
+
